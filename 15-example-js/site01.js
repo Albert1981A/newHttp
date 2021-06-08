42d26622f7c795e103fbbb5c092485aa80cc55fa
Albert Abuav <albrtabo@gmail.com>
2021-06-08 13:11:20 +0300

    Commit js window

---
+++ 15-example-js/site01.js
@@ -0,0 +1,9 @@
+
+// Literal Object
+let c1 = {"id":1, "name":"Mitzi", "age":4, "weight":3.5}
+console.log(c1);
+console.log(c1.id);
+console.log(c1.name);
+console.log(c1.age, c1.weight);
+
+// 2:25
